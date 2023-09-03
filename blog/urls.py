c031db295d06e369b95bf55b87cc09d887e45aeb
ArCaneSec <arcanebughunter@gmail.com>
2023-09-03 22:02:48 +0330

    completed email section, added comment functionality, chapter 2 conpleted.

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
         views.post_details,
         name="post_details",
     ),
+    path("<int:post_id>/share/", views.post_email, name="post_share"),
+    path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .models import Post
-from .forms import EmailPostForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from .models import Post, Comment
+from .forms import EmailPostForm, AddCommentForm
 
 # Create your views here.
 
@@ -32,9 +34,14 @@ def post_details(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+    comments = post.comments.filter(active=True)
+    form = AddCommentForm()
 
-    print(post.title)
-    return render(request, "blog/post/detail.html", {"post": post})
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post, "comments": comments, "form": form},
+    )
 
 
 class PostListView(ListView):
@@ -43,15 +50,48 @@ class PostListView(ListView):
     paginate_by = 2
     template_name = "blog/post/list.html"
 
-def post_or_share_email(request, post_id):
-    post = get_object_or_404(Post, post_details)
+
+def post_email(request, post_id):
+    """
+    This view is for sharing posts via email.
+    """
+    post = get_object_or_404(Post, id=post_id)
+    sent = False
 
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
+           
             data = form.cleaned_data
-        
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            print(post.get_absolute_url())
+            subject = f"{data['name']} has recommended you to read {post.title}!"
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{data['name']}'s comments: {data['comment']}"
+            )
+            send_mail(subject, message, data["email"], [data["to"]])
+            sent = True
     else:
         form = EmailPostForm()
 
-    return render(request, "blog/post/share.html", {"post": post, "form": form})
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = AddCommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
+    )
